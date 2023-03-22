9e6e3538b205aa4142ede7e9680ab5f6ac17579f
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-03-22 10:46:36 +0700

    number data type

---
+++ 2-number-data-type/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu:", 1)
+	fmt.Println("Dua:", 2)
+	fmt.Println("Tiga Koma Lima:", 3.5)
+}
+
+// there are two types: Integer and Floating Point
+// Integer: int8, int16, int32, int64
+// Unsigned Integer: unit8, uint16, uint32, uint64 => start from zero
+// Floating Point: float32, float64, complex64, complex128
+// Alias: byte => uint8, rune => int32, int => min. int32, uint => min. uint32

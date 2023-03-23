a7cac24e996738fc6048469ae7cab17eedef8cde
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-03-23 21:17:58 +0700

    string

---
+++ 4-string/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhamad Ali Nur Zaman")
+	fmt.Println(len("Ali"))   // length of string
+	fmt.Println("Muhamad"[3]) // represents byte of character of 'a'
+}

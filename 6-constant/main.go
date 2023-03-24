c0f0448b9b0d8890e57f3c4ad5ec131a15b954a9
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-03-24 22:00:53 +0700

    constant

---
+++ 6-constant/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName = "Muhamad Ali"
+		lastName  = "Nur Zaman"
+		age       = 30
+	)
+
+	fmt.Println(firstName, lastName)
+	fmt.Println(age)
+}

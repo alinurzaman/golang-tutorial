72bb8d4189cae45471f098504f8a3197ffe26041
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-07 09:56:55 +0700

    error interface

---
+++ 38-error-interface/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func calculate(value int, divider int) (int, error) {
+	if divider == 0 {
+		return 0, errors.New("divider shouldn't be 0")
+	} else {
+		result := value / divider
+		return result, nil
+	}
+}
+func main() {
+	result, e := calculate(100, 0)
+	if e == nil {
+		fmt.Println(result)
+	} else {
+		fmt.Print(e)
+	}
+}

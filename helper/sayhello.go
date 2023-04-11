6737f6fae1a542e91585a7d01b45b91ae165c079
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-04-11 09:36:57 +0700

    access modifier

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,13 @@ package helper
 
 import "fmt"
 
+var version = 1                    //if variable or function start with lowercase then it cannot be accessed from outside
+var Application = "Learn Golang" //if variable or function start with uppercase then it can be accessed from outside
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+func sayGoodbye(name string) {
+	fmt.Println("Goodbye", name)
 }
